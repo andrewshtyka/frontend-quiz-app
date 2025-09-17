05e2fe50f727ec147b334756e1cabb27a1656cce
Andrew Shtyka <andrii.shtyka@gmail.com>
2025-09-17 13:01:22 +0300

    add prettier

--- src/scripts/populate-data.js
+++ src/scripts/populate-data.js
@@ -1,266 +1,256 @@
-import * as Header from "./header";
-import * as Progress from "./progress-bar";
-import * as Result from "./result";
-import * as Radio from "./radio";
-import * as Validate from "./validate";
-import * as StorageQuiz from "./local-storage-quiz";
-import * as AnimateStartScreen from "./anim-screen-quiz";
-import * as AnimateButtons from "./anim-buttons";
+import * as Header from "./header"
+import * as Progress from "./progress-bar"
+import * as Result from "./result"
+import * as Radio from "./radio"
+import * as Validate from "./validate"
+import * as StorageQuiz from "./local-storage-quiz"
+import * as AnimateStartScreen from "./anim-screen-quiz"
+import * as AnimateButtons from "./anim-buttons"
 
 // ================================================================================
 //
 // VARIABLES
-const errorMessage = document.getElementById("error-answer");
-const questionNumber = document.querySelector("#screen-quiz [data-number]");
-const questionText = document.querySelector("#screen-quiz [data-question]");
-const optionA = document.querySelector("[for='option-a'] [data-answer]");
-const optionB = document.querySelector("[for='option-b'] [data-answer]");
-const optionC = document.querySelector("[for='option-c'] [data-answer]");
-const optionD = document.querySelector("[for='option-d'] [data-answer]");
-const buttonSubmit = document.getElementById("button-submit");
-const screenStart = document.getElementById("screen-start");
-const screenQuiz = document.getElementById("screen-quiz");
-const screenResult = document.getElementById("screen-result");
+const errorMessage = document.getElementById("error-answer")
+const questionNumber = document.querySelector("#screen-quiz [data-number]")
+const questionText = document.querySelector("#screen-quiz [data-question]")
+const optionA = document.querySelector("[for='option-a'] [data-answer]")
+const optionB = document.querySelector("[for='option-b'] [data-answer]")
+const optionC = document.querySelector("[for='option-c'] [data-answer]")
+const optionD = document.querySelector("[for='option-d'] [data-answer]")
+const buttonSubmit = document.getElementById("button-submit")
+const screenStart = document.getElementById("screen-start")
+const screenQuiz = document.getElementById("screen-quiz")
+const screenResult = document.getElementById("screen-result")
 
-const radios = document.querySelectorAll(
-  'input[type="radio"][name="option-answer"]'
-);
-const labels = document.querySelectorAll("#radios label[for]");
+const radios = document.querySelectorAll('input[type="radio"][name="option-answer"]')
+const labels = document.querySelectorAll("#radios label[for]")
 
 // ================================================================================
 //
 // STATE
-let userAnswer = null;
-let isChecked = false;
-let userScore = 0;
-let isCorrect = 0;
-let currentQ = 0;
-let currentData = null;
-let amountQuestions = 0;
-let currentRadio = null;
+let userAnswer = null
+let isChecked = false
+let userScore = 0
+let isCorrect = 0
+let currentQ = 0
+let currentData = null
+let amountQuestions = 0
+let currentRadio = null
 
 // ================================================================================
 //
 // RADIOS
-labels.forEach((label) => {
-  label.addEventListener("click", (e) => {
-    e.preventDefault();
-    currentRadio = e.currentTarget;
-    isChecked = Radio.radioCheck(label, radios);
-    userAnswer = label.querySelector("[data-answer]").textContent;
-    AnimateButtons.animErrorMessageHide(errorMessage);
-  });
+labels.forEach(label => {
+	label.addEventListener("click", e => {
+		e.preventDefault()
+		currentRadio = e.currentTarget
+		isChecked = Radio.radioCheck(label, radios)
+		userAnswer = label.querySelector("[data-answer]").textContent
+		AnimateButtons.animErrorMessageHide(errorMessage)
+	})
 
-  label.addEventListener("keydown", (e) => {
-    if (e.code === "Enter" || e.code === "Space") {
-      e.preventDefault();
-      currentRadio = e.currentTarget;
-      isChecked = Radio.radioCheck(label, radios);
-      userAnswer = label.querySelector("[data-answer]").textContent;
-      AnimateButtons.animErrorMessageHide(errorMessage);
-    }
-  });
-});
+	label.addEventListener("keydown", e => {
+		if (e.code === "Enter" || e.code === "Space") {
+			e.preventDefault()
+			currentRadio = e.currentTarget
+			isChecked = Radio.radioCheck(label, radios)
+			userAnswer = label.querySelector("[data-answer]").textContent
+			AnimateButtons.animErrorMessageHide(errorMessage)
+		}
+	})
+})
 
 // ================================================================================
 //
 // SUBMIT
-let isSubmitted = false;
+let isSubmitted = false
 
 buttonSubmit.addEventListener("click", () => {
-  if (buttonSubmit.disabled) return;
+	if (buttonSubmit.disabled) return
 
-  if (!isSubmitted) {
-    submitCheck(isChecked);
-  } else {
-    nextQuestion();
-  }
+	if (!isSubmitted) {
+		submitCheck(isChecked)
+	} else {
+		nextQuestion()
+	}
 
-  buttonSubmit.disabled = true;
-  setTimeout(() => {
-    buttonSubmit.disabled = false;
-  }, 500);
-});
+	buttonSubmit.disabled = true
+	setTimeout(() => {
+		buttonSubmit.disabled = false
+	}, 500)
+})
 
 // submit button logic
 function submitCheck(status) {
-  if (!status) {
-    AnimateButtons.animErrorMessage(errorMessage);
-    return;
-  }
+	if (!status) {
+		AnimateButtons.animErrorMessage(errorMessage)
+		return
+	}
 
-  isSubmitted = true;
+	isSubmitted = true
 
-  if (amountQuestions - currentQ === 1) {
-    AnimateButtons.animateButtonText(buttonSubmit, "Show Result");
-  } else {
-    AnimateButtons.animateButtonText(buttonSubmit, "Next Question");
-  }
+	if (amountQuestions - currentQ === 1) {
+		AnimateButtons.animateButtonText(buttonSubmit, "Show Result")
+	} else {
+		AnimateButtons.animateButtonText(buttonSubmit, "Next Question")
+	}
 
-  isCorrect = Validate.validateAnswer(
-    userAnswer,
-    currentData.questions[currentQ].answer,
-    currentRadio,
-    labels
-  );
-  userScore += isCorrect;
-  Result.updateScore(userScore, amountQuestions);
+	isCorrect = Validate.validateAnswer(userAnswer, currentData.questions[currentQ].answer, currentRadio, labels)
+	userScore += isCorrect
+	Result.updateScore(userScore, amountQuestions)
 
-  radioBlock();
+	radioBlock()
 }
 
 // next question button logic
 function nextQuestion() {
-  currentQ++;
+	currentQ++
 
-  Validate.removeValidationStyles(labels);
-  radioUnblock();
+	Validate.removeValidationStyles(labels)
+	radioUnblock()
 
-  if (currentQ < amountQuestions) {
-    updateData(currentQ);
-    Radio.resetRadios(radios);
-    userAnswer = null;
-    isChecked = false;
-    isSubmitted = false;
-    AnimateButtons.animateButtonText(buttonSubmit, "Submit Answer");
-  } else {
-    AnimateStartScreen.animHideShowScreen(screenQuiz, screenResult);
-    Radio.resetRadios(radios);
-    currentQ = 0;
-    userAnswer = null;
-    isChecked = false;
-    isSubmitted = false;
-    AnimateButtons.animateButtonText(buttonSubmit, "Submit Answer");
-  }
+	if (currentQ < amountQuestions) {
+		updateData(currentQ)
+		Radio.resetRadios(radios)
+		userAnswer = null
+		isChecked = false
+		isSubmitted = false
+		AnimateButtons.animateButtonText(buttonSubmit, "Submit Answer")
+	} else {
+		AnimateStartScreen.animHideShowScreen(screenQuiz, screenResult)
+		Radio.resetRadios(radios)
+		currentQ = 0
+		userAnswer = null
+		isChecked = false
+		isSubmitted = false
+		AnimateButtons.animateButtonText(buttonSubmit, "Submit Answer")
+	}
 }
 
 // ================================================================================
 //
 // TOPIC SELECT
 export function handleTopicSelect(e) {
-  const isInput =
-    e.type === "click" || e.code === "Enter" || e.code === "Space";
-  if (!isInput) return;
+	const isInput = e.type === "click" || e.code === "Enter" || e.code === "Space"
+	if (!isInput) return
 
-  const topicName = e.currentTarget.textContent.trim();
+	const topicName = e.currentTarget.textContent.trim()
 
-  loadTopic(topicName);
+	loadTopic(topicName)
 }
 
 // ================================================================================
 //
 // LOAD TOPIC
 async function loadTopic(topicName) {
-  const dataArray = await getData();
-  if (!dataArray) return;
+	const dataArray = await getData()
+	if (!dataArray) return
 
-  const quiz = dataArray.quizzes.find((q) => q.title === topicName);
-  if (!quiz) return;
+	const quiz = dataArray.quizzes.find(q => q.title === topicName)
+	if (!quiz) return
 
-  currentData = quiz;
-  amountQuestions = quiz.questions.length;
+	currentData = quiz
+	amountQuestions = quiz.questions.length
 
-  Header.changeTopicIcon(quiz.icon, quiz);
-  updateData(currentQ);
+	Header.changeTopicIcon(quiz.icon, quiz)
+	updateData(currentQ)
 
-  AnimateStartScreen.animShowHeader();
-  AnimateStartScreen.animHideShowScreen(screenStart, screenQuiz);
+	AnimateStartScreen.animShowHeader()
+	AnimateStartScreen.animHideShowScreen(screenStart, screenQuiz)
 }
 
 // load data from localStorage
 export async function restoreQuiz(savedData) {
-  const dataArray = await getData();
-  if (!dataArray) return;
+	const dataArray = await getData()
+	if (!dataArray) return
 
-  const quiz = dataArray.quizzes.find((q) => q.title === savedData.topic);
-  if (!quiz) return;
+	const quiz = dataArray.quizzes.find(q => q.title === savedData.topic)
+	if (!quiz) return
 
-  currentData = quiz;
-  amountQuestions = quiz.questions.length;
-  currentQ = savedData.questionIndex || 0;
-  userScore = savedData.userScore || 0;
+	currentData = quiz
+	amountQuestions = quiz.questions.length
+	currentQ = savedData.questionIndex || 0
+	userScore = savedData.userScore || 0
 
-  Header.changeTopicIcon(quiz.icon, quiz);
+	Header.changeTopicIcon(quiz.icon, quiz)
 
-  updateData(currentQ);
-  Result.updateScore(userScore, amountQuestions);
+	updateData(currentQ)
+	Result.updateScore(userScore, amountQuestions)
 
-  AnimateStartScreen.animHideShowScreen(screenStart, screenQuiz);
-  AnimateStartScreen.animShowHeader();
+	AnimateStartScreen.animHideShowScreen(screenStart, screenQuiz)
+	AnimateStartScreen.animShowHeader()
 }
 
 // ================================================================================
 //
 // UPDATE QUESTION
 function updateData(index) {
-  const currentQuestion = currentData.questions[index];
+	const currentQuestion = currentData.questions[index]
 
-  StorageQuiz.saveQuizState(index, userScore, currentData.title);
+	StorageQuiz.saveQuizState(index, userScore, currentData.title)
 
-  if (index !== 0) {
-    // questions after 1st
-    AnimateStartScreen.animHideShowScreenQuiz(screenQuiz, () => {
-      questionNumber.textContent = `Question ${
-        index + 1
-      } of ${amountQuestions}`;
-      Progress.progressBar(index);
-      questionText.textContent = currentQuestion.question;
-      optionA.textContent = currentQuestion.options[0];
-      optionB.textContent = currentQuestion.options[1];
-      optionC.textContent = currentQuestion.options[2];
-      optionD.textContent = currentQuestion.options[3];
-    });
-  } else {
-    // only 1st question (no animation)
-    questionNumber.textContent = `Question ${index + 1} of ${amountQuestions}`;
-    Progress.progressBar(index);
-    questionText.textContent = currentQuestion.question;
-    optionA.textContent = currentQuestion.options[0];
-    optionB.textContent = currentQuestion.options[1];
-    optionC.textContent = currentQuestion.options[2];
-    optionD.textContent = currentQuestion.options[3];
-  }
+	if (index !== 0) {
+		// questions after 1st
+		AnimateStartScreen.animHideShowScreenQuiz(screenQuiz, () => {
+			questionNumber.textContent = `Question ${index + 1} of ${amountQuestions}`
+			Progress.progressBar(index)
+			questionText.textContent = currentQuestion.question
+			optionA.textContent = currentQuestion.options[0]
+			optionB.textContent = currentQuestion.options[1]
+			optionC.textContent = currentQuestion.options[2]
+			optionD.textContent = currentQuestion.options[3]
+		})
+	} else {
+		// only 1st question (no animation)
+		questionNumber.textContent = `Question ${index + 1} of ${amountQuestions}`
+		Progress.progressBar(index)
+		questionText.textContent = currentQuestion.question
+		optionA.textContent = currentQuestion.options[0]
+		optionB.textContent = currentQuestion.options[1]
+		optionC.textContent = currentQuestion.options[2]
+		optionD.textContent = currentQuestion.options[3]
+	}
 
-  isChecked = false;
+	isChecked = false
 }
 
 // ================================================================================
 //
 // FETCH DATA
 export async function getData() {
-  try {
-    const response = await fetch("/data.json");
-    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-    return await response.json();
-  } catch (err) {
-    console.error("Fetch error:", err);
-  }
+	try {
+		const response = await fetch("/data.json")
+		if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`)
+		return await response.json()
+	} catch (err) {
+		console.error("Fetch error:", err)
+	}
 }
 
 // ================================================================================
 //
 // RESET QUIZ STATE
 export function resetQuiz() {
-  userAnswer = null;
-  isChecked = false;
-  userScore = 0;
-  currentQ = 0;
-  errorMessage.classList.add("is-hidden");
-  Radio.resetRadios(radios);
-  currentData = null;
+	userAnswer = null
+	isChecked = false
+	userScore = 0
+	currentQ = 0
+	errorMessage.classList.add("is-hidden")
+	Radio.resetRadios(radios)
+	currentData = null
 }
 
 // ================================================================================
 //
 // BLOCK / UNBLOCK RADIOS WHEN SUBMITTED
 export function radioBlock() {
-  labels.forEach((label) => {
-    label.style.pointerEvents = "none";
-  });
+	labels.forEach(label => {
+		label.style.pointerEvents = "none"
+	})
 }
 
 export function radioUnblock() {
-  labels.forEach((label) => {
-    label.style.pointerEvents = "auto";
-  });
+	labels.forEach(label => {
+		label.style.pointerEvents = "auto"
+	})
 }
